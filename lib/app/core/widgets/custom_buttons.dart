329511e7b7e532312044b257db98edeab0645acd
Sivanandh P P <sivanandhpp@gmail.com>
2025-07-11 12:28:47 +0530

    Refactor custom buttons implementation; move to core directory and update import paths

--- lib/app/core/widgets/custom_buttons.dart
+++ lib/app/core/widgets/custom_buttons.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import '../core/themes/app_colors.dart';
-import '../core/themes/app_text_styles.dart';
+import '../themes/app_colors.dart';
+import '../themes/app_text_styles.dart';
 
 class CustomButtons {
   // Primary Elevated Button
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,5 +1,5 @@
 import 'package:bevco/app/core/constants/app_strings.dart';
-import 'package:bevco/app/widgets/custom_buttons.dart';
+import 'package:bevco/app/core/widgets/custom_buttons.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
