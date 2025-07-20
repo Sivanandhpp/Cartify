16e45bb974196d583fd14c1490a06aaa5ed8786e
Sivanandh PP <sivanandhpp@gmail.com>
2025-07-20 20:21:32 +0530

    STABLE V1 refactor: remove user panel modules and their routes

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -16,20 +16,6 @@ import '../modules/splash/bindings/splash_binding.dart';
 import '../modules/splash/views/splash_view.dart';
 import '../modules/user_dashboard/bindings/user_dashboard_binding.dart';
 import '../modules/user_dashboard/views/user_dashboard_view.dart';
-import '../modules/user_panel/cart/bindings/user_panel_cart_binding.dart';
-import '../modules/user_panel/cart/views/user_panel_cart_view.dart';
-import '../modules/user_panel/categories/bindings/user_panel_categories_binding.dart';
-import '../modules/user_panel/categories/views/user_panel_categories_view.dart';
-import '../modules/user_panel/dashboard/bindings/user_panel_dashboard_binding.dart';
-import '../modules/user_panel/dashboard/views/user_panel_dashboard_view.dart';
-import '../modules/user_panel/home/bindings/user_panel_home_binding.dart';
-import '../modules/user_panel/home/views/user_panel_home_view.dart';
-import '../modules/user_panel/offers/bindings/user_panel_offers_binding.dart';
-import '../modules/user_panel/offers/views/user_panel_offers_view.dart';
-import '../modules/user_panel/profile/bindings/user_panel_profile_binding.dart';
-import '../modules/user_panel/profile/views/user_panel_profile_view.dart';
-import '../modules/user_panel/wishlist/bindings/user_panel_wishlist_binding.dart';
-import '../modules/user_panel/wishlist/views/user_panel_wishlist_view.dart';
 
 // Routes barrel export file
 // Usage: import 'package:cartify/app/routes/app_pages.dart';
@@ -78,48 +64,6 @@ class AppPages {
       name: _Paths.PRODUCT_SHEET,
       page: () => const ProductSheetView(),
       binding: ProductSheetBinding(),
-    ),
-    GetPage(
-      name: _Paths.CART,
-      page: () => const CartView(),
-      binding: CartBinding(),
-      children: [
-        GetPage(
-          name: _Paths.USER_PANEL_CART,
-          page: () => const UserPanelCartView(),
-          binding: UserPanelCartBinding(),
-        ),
-      ],
-    ),
-    GetPage(
-      name: _Paths.USER_PANEL_DASHBOARD,
-      page: () => const UserPanelDashboardView(),
-      binding: UserPanelDashboardBinding(),
-    ),
-    GetPage(
-      name: _Paths.USER_PANEL_PROFILE,
-      page: () => const UserPanelProfileView(),
-      binding: UserPanelProfileBinding(),
-    ),
-    GetPage(
-      name: _Paths.USER_PANEL_OFFERS,
-      page: () => const UserPanelOffersView(),
-      binding: UserPanelOffersBinding(),
-    ),
-    GetPage(
-      name: _Paths.USER_PANEL_WISHLIST,
-      page: () => const UserPanelWishlistView(),
-      binding: UserPanelWishlistBinding(),
-    ),
-    GetPage(
-      name: _Paths.USER_PANEL_CATEGORIES,
-      page: () => const UserPanelCategoriesView(),
-      binding: UserPanelCategoriesBinding(),
-    ),
-    GetPage(
-      name: _Paths.USER_PANEL_HOME,
-      page: () => const UserPanelHomeView(),
-      binding: UserPanelHomeBinding(),
     ),
   ];
 }
