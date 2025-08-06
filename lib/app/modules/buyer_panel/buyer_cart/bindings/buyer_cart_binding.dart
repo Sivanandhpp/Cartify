9269f32d9f304f220c15e6688de4515752b5c039
Sivanandh PP <sivanandhpp@gmail.com>
2025-08-06 21:04:44 +0530

    refactor: remove buyer panel wishlist and related components
    - Deleted BuyerWishlistView and its controller to streamline the buyer panel.
    - Removed QuantitySelectorWidget as it was no longer needed.
    - Eliminated ProductSheetController and associated product sheet components.
    - Cleaned up product sheet widgets including ExpandableDetailsSection, ProductImageCarousel, ProductInfoSection, and SellerDetailsSection.
    - Commented out routes related to buyer panel features to prevent navigation errors.

--- lib/app/modules/buyer_panel/buyer_cart/bindings/buyer_cart_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import '../controllers/buyer_cart_controller.dart';
-
-class BuyerCartBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut<BuyerCartController>(() => BuyerCartController());
-  }
-}
